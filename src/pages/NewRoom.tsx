29591c99b23b24eea516e8a145f5fca411bdd5a3
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-05-07 12:15:09 -0300

    Componente para criar uma sala nova. Foi implementado o fluxo para o redirecionamento da sala

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import {FormEvent, useState} from 'react'
 
 import logoImg from '../assets/images/logo.svg';
 import illustrationImg from '../assets/images/illustration.svg';
@@ -6,10 +7,32 @@ import { Button } from '../components/Button';
 
 import '../styles/auth.scss'
 import { useAuth } from '../hooks/useAuth';
+import {database} from '../services/firebase';
+import { useNavigate } from 'react-router-dom';
 
 export const NewRoomm = () => {
 
   const {user} = useAuth();
+
+  const navigate = useNavigate()
+  const [newRoom, setNewRoom] = useState('');
+  const handleCreateRoom = async (event: FormEvent) => {
+    event.preventDefault();
+
+    if(newRoom.trim() === '') {
+      return
+    }
+
+    const roomRef = database.ref('rooms')
+
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id,
+    })
+
+    navigate(`/rooms/${firebaseRoom.key}`)
+
+  }
 
   return (
     <div id="page-auth">
@@ -22,10 +45,12 @@ export const NewRoomm = () => {
         <div className='main-content'>
           <img src={logoImg} alt="letmeask" />
           <h2>Crie um nova sala</h2>
-          <form>
+          <form onSubmit={handleCreateRoom}>
             <input
               type="text"
               placeholder='Nome da sala'
+              onChange={({target}) => setNewRoom(target.value)}
+              value={newRoom}
             />
             <Button type='submit'>Criar sala</Button>
           </form>
