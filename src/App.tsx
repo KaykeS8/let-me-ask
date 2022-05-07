e99af77b738b6acd5e0aff25a361e4c225709f82
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-05-07 12:17:20 -0300

    Implementação de mais uma rota

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NewRoomm } from "./pages/NewRoom";
 import { AuthContextProvider } from "./contexts/AuthContext";
+import { Room } from "./pages/Room";
 
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/rooms/new" element={<NewRoomm />} />
+          <Route path="/rooms/:id" element={<Room />} />
         </Routes>
       </AuthContextProvider>
     </BrowserRouter>
