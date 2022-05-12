45ea8617aafe3645dc81cd4114a0db5a77d760a2
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-05-12 20:16:20 -0300

    Foi implementado uma nova rota na paginia, que direciona para a página de Administrador

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Home } from "./pages/Home";
 import { NewRoomm } from "./pages/NewRoom";
 import { AuthContextProvider } from "./contexts/AuthContext";
 import { Room } from "./pages/Room";
+import { AdminRoom } from "./pages/AdminRoom";
 
 
 
@@ -16,6 +17,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/rooms/new" element={<NewRoomm />} />
           <Route path="/rooms/:id" element={<Room />} />
+          <Route path="/admin/rooms/:id" element={<AdminRoom />} />
         </Routes>
       </AuthContextProvider>
     </BrowserRouter>
